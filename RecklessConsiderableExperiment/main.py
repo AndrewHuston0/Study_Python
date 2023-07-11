8387b2a7ddbaff5b32aced09597a58f031cf757d
AndrewHuston0 <prototypezodiac@gmail.com>
2023-07-11 18:40:14 +0900

    first commit for python study

---
+++ RecklessConsiderableExperiment/main.py
@@ -0,0 +1,18 @@
+print("Hello world!")
+
+a = 2  # 0x0001
+b = 3  # 0x0002
+c = a + b  # 0x0003
+print(c)
+
+# variable은 메모리 주소에 해당 값을 집어 넣는 것
+# python read code top to bottom
+c = 1
+print(c)
+a = 10
+b = 1
+print(c)
+# the value of "c" didn't change
+
+# if you want to create a variable that variable has a space on each name, you shouldn't write the space
+# you shouldn't start variable with numbers. Start just with text
